7602ff221b795d577b808aaab326ca610d9cc9bb
jesman1 <jesman9@hotmail.com>
2022-04-02 00:46:06 -0400

    Add files via upload

---
+++ osu! Logo template.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         osu! Logo template - Canadian ver
+// @namespace    http://tampermonkey.net/
+// @version      0.1.1
+// @description  try to take over the canvas! Edited by Jesman1 for /r/PlaceCanada
+// @author       oralekin
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://i.imgur.com/nTWJLwJ.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
+
